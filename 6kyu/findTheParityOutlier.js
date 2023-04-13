ffe7e04cb3beb9af8d32c718648c92a513d471b8
Dallair220 <93786532+Dallair220@users.noreply.github.com>
2023-04-13 11:00:52 +0200

    Add new kata

---
+++ 6kyu/findTheParityOutlier.js
@@ -0,0 +1,31 @@
+// https://www.codewars.com/kata/5526fc09a1bbd946250002dc
+
+// DESCRIPTION:
+// You are given an array (which will have a length of at least 3, but could be very large) containing integers.
+// The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N.
+// Write a method that takes the array as an argument and returns this "outlier" N.
+
+// Examples
+// [2, 4, 0, 100, 4, 11, 2602, 36]
+// Should return: 11 (the only odd number)
+
+// [160, 3, 1719, 19, 11, 13, -21]
+// Should return: 160 (the only even number)
+
+// My Solution:
+function findOutlier(integers) {
+  let sumOdd = 0;
+  let sumEven = 0;
+  let lastIndexOdd = 0;
+  let lastIndexEven = 0;
+  for (let i = 0; i < integers.length; i++) {
+    if (integers[i] % 2) {
+      sumOdd++;
+      lastIndexOdd = i;
+    } else {
+      sumEven++;
+      lastIndexEven = i;
+    }
+  }
+  return sumOdd > sumEven ? integers[lastIndexEven] : integers[lastIndexOdd];
+}
